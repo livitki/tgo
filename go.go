abf266e3b8f3c885e6e2d27080ca730a8037adbc
Mihail Livitchi <livitki@gmail.com>
2016-02-18 11:00:25 +0100

    f

---
+++ go.go
@@ -0,0 +1,13 @@
+package main
+
+import ("fmt"
+	"net/http")
+
+func main(){
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprint(w, "hello")
+	})
+
+	http.ListenAndServe(":8082", nil)
+}
